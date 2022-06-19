67ff5ef8dc74fb2325c4206a6876b48758a11e5b
Peter <41585899+pbanigo@users.noreply.github.com>
2022-06-19 17:47:00 +0100

    Items displaying on front page

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,14 @@
+const axios = require('axios');//http client used for making client & server side http requests in node
 
-exports.homeRoutes= function(req, res) {//this listens for a get request for "/" the homepage
-  
-  res.render('index');
+exports.homeRoutes= function(req, res) {
+    // Make a get request to /api/users
+    axios.get(`${process.env.BASE_URI}:${process.env.PORT}/api/drugs`)//get request to pull drugs
+        .then(function(response){
+            res.render('index', { drugs : response.data });// response from API request stored as drugs
+        })
+        .catch(err =>{
+            res.send(err);
+        })
 }
 
 exports.addDrug =  function(req, res) {//this listens for a get request for "/add-drug" from any hyperlink
